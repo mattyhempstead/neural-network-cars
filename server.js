0c125dc51e30f045ee8fa84fe86b5c0c49478eec
Matty <shadowflame919@gmail.com>
2016-11-08 00:09:14 +1100

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
 
 http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+  console.log('Express server listening on ' + app.get('ip') + " and " + app.get('port'));
 });
 
 app.get('/', function (req, res) {
